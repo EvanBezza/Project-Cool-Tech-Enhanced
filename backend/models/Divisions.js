0e83a0f5a5f34eb23fc256da350eb12e61cad2d0
Evan Bezuidenhout <64897765+EvanBezza@users.noreply.github.com>
2023-05-24 11:19:37 +0200

    re-doing Master

--- backend/models/Divisions.js
+++
@@ -1,9 +0,0 @@
-// Division.js
-const mongoose = require('mongoose');
-
-const DivisionSchema = new mongoose.Schema({
-  name: String,
-  credentialRepository: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Credential' }],
-});
-
-module.exports = mongoose.model('Division', DivisionSchema);
